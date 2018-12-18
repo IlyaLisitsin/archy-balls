18c5c3dddf6a4119c0cf1bc3b461208f8f87da41
Ilya Savchuk <ilyas.sporichev@gmail.com>
2018-12-18 03:26:39 +0300

    initital commit

--- src/js/MouseTracker.js
+++ src/js/MouseTracker.js
@@ -1,23 +1,23 @@
 class MouseTracker {
   constructor(el) {
-    this._el = el;
-    this.x = 0;
-    this.y = 0;
-    this.l = false;
-    this.r = false;
+    this._el = el
+    this.x = 0
+    this.y = 0
+    // this.l = false
+    // this.r = false
 
-    el.addEventListener('mousemove', this._track.bind(this));
-    el.addEventListener('mouseup', this._track.bind(this));
-    el.addEventListener('mousedown', this._track.bind(this));
+    el.addEventListener('mousemove', this._track.bind(this))
+    el.addEventListener('mouseup', this._track.bind(this))
+    el.addEventListener('mousedown', this._track.bind(this))
   }
 
   _track(e) {
-    var rect = this._el.getBoundingClientRect();
-    this.x = e.clientX - rect.left;
-    this.y = e.clientY - rect.top;
-    this.l = !!(e.buttons & 1);
-    this.r = !!(e.buttons & 2);
+    const rect = this._el.getBoundingClientRect()
+    this.x = e.clientX - rect.left
+    this.y = e.clientY - rect.top
+    // this.l = !!(e.buttons & 1)
+    // this.r = !!(e.buttons & 2)
   }
 }
 
-export default MouseTracker;
+export default MouseTracker
--- src/js/main.js
+++ src/js/main.js
@@ -1,80 +1,85 @@
 import Vec2 from './Vec2'
 import MouseTracker from './MouseTracker'
 
-CanvasRenderingContext2D.prototype.circle = function(x, y, r) {
-    this.moveTo(x + r, y);
-    this.arc(x, y, r, 0, 2 * Math.PI);
-};
-
-class Ball {
-    constructor(pos, vel) {
-        this.pos = pos;
-        this.vel = vel;
-    }
-
-    move() {
-        this.pos = this.pos.add(this.vel);
-    }
+const rand = function(max, min = 0) {
+    const diff = max - min
+    return Math.floor(Math.random() * diff) + min
 }
 
-var gWidth = 320;
-var gHeight = 240;
-
-var balls = [];
-for (var i = 0; i < 10; i++) {
-    var pos = new Vec2(Math.rand(gWidth), Math.rand(gHeight));
-    var vel = new Vec2(Math.rand(-99, 100), Math.rand(-99, 100)).norm().mul(5);
-    var ball = new Ball(pos, vel);
-    balls.push(ball);
-}
-
-var canvas = createCanvas(gWidth, gHeight);
-
-var mouse = new MouseTracker(canvas);
-
-document.body.appendChild(canvas);
-var ctx = canvas.getContext('2d');
-
-loop();
-
-function loop() {
-    var m = new Vec2(mouse.x, mouse.y);
-    for (var i = 0; i < balls.length; i++) {
-        var d = balls[i].pos.sub(m).norm();
-        var l = balls[i].vel.len();
-        balls[i].vel = balls[i].vel.add(d).norm().mul(l);
-
-        if (balls[i].pos.x < 0) {
-            balls[i].vel.x = Math.abs(balls[i].vel.x);
-        }
-        else if (balls[i].pos.x > gWidth) {
-            balls[i].vel.x = Math.abs(balls[i].vel.x) * -1;
-        }
-        if (balls[i].pos.y < 0) {
-            balls[i].vel.y = Math.abs(balls[i].vel.y);
-        }
-        else if (balls[i].pos.y > gHeight) {
-            balls[i].vel.y = Math.abs(balls[i].vel.y) * -1;
-        }
-
-        balls[i].move();
-    }
-
-    ctx.fillStyle = '#000';
-    ctx.fillRect(0, 0, gWidth, gHeight);
-    ctx.strokeStyle = '#FFF';
-    ctx.beginPath();
-    for (var i = 0; i < balls.length; i++) {
-        ctx.circle(balls[i].pos.x, balls[i].pos.y, 10);
-    }
-    ctx.stroke();
-
-    requestAnimationFrame(loop);
+const circle = function(x, y, r) {
+    canvas.getContext('2d').moveTo(x + r, y)
+    canvas.getContext('2d').arc(x, y, r, 0, 2 * Math.PI)
 }
 
 function createCanvas(width, height) {
-    var canvas = document.createElement('canvas');
-    canvas.width = width;
-    canvas.height = height;
-    return canvas;
+    const canvas = document.createElement('canvas')
+    canvas.width = width
+    canvas.height = height
+    return canvas
+}
+
+class Ball {
+    constructor(pos, vel) {
+        this.pos = pos
+        this.vel = vel
+    }
+
+    move() {
+        this.pos = this.pos.add(this.vel)
+    }
+}
+
+const gWidth = 320
+const gHeight = 240
+
+const balls = []
+for (let i = 0; i < 10; i++) {
+    const pos = new Vec2(rand(gWidth), rand(gHeight))
+    const vel = new Vec2(rand(-99, 100), rand(-99, 100)).norm().mul(5)
+    const ball = new Ball(pos, vel)
+    balls.push(ball)
+}
+
+const canvas = createCanvas(gWidth, gHeight)
+
+const mouse = new MouseTracker(canvas)
+
+document.body.appendChild(canvas)
+const ctx = canvas.getContext('2d')
+
+loop(ctx)
+
+function loop() {
+    const m = new Vec2(mouse.x, mouse.y)
+    balls.forEach(ball => {
+
+        const d = ball.pos.sub(m).norm()
+        const l = ball.vel.len()
+        ball.vel = ball.vel.add(d).norm().mul(l)
+
+        if (ball.pos.x < 0) {
+            ball.vel.x = Math.abs(ball.vel.x)
+        }
+        else if (ball.pos.x > gWidth) {
+            ball.vel.x = Math.abs(ball.vel.x) * -1
+        }
+        if (ball.pos.y < 0) {
+            ball.vel.y = Math.abs(ball.vel.y)
+        }
+        else if (ball.pos.y > gHeight) {
+            ball.vel.y = Math.abs(ball.vel.y) * -1
+        }
+
+        ball.move()
+    })
+
+    ctx.fillStyle = '#000'
+    ctx.fillRect(0, 0, gWidth, gHeight)
+    ctx.strokeStyle = '#FFF'
+    ctx.beginPath()
+
+    balls.forEach(ball => circle(ball.pos.x, ball.pos.y, 10))
+    ctx.stroke()
+
+    requestAnimationFrame(loop)
 }
